74295055dd018eb5d393a7fd3a65bf537174aae3
Kappanneo <pannacciclaudio@gmail.com>
2018-08-16 18:51:21 +0200

    Exaustionem.

--- app/src/main/java/disorganizzazione/fency/LogFragment.kt
+++ app/src/main/java/disorganizzazione/fency/LogFragment.kt
@@ -5,7 +5,6 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import kotlinx.android.synthetic.main.fragment_log.*
 
 class LogFragment : Fragment(){
 
@@ -15,11 +14,9 @@ class LogFragment : Fragment(){
         return inflater.inflate(R.layout.fragment_log, container, false)
     }
 
-    fun append(tag: Int) {
-        logView.text = "${logView.text} ${getText(tag)}"
-    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
-    fun submit(tag: Int) {
-        logView.text = "${logView.text}\n${getText(tag)}"
+        (activity as DuelModeActivity).onLogCreated()
     }
 }
